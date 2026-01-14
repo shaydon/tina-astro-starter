b9c9e004eccd45342217b7733ec07adcb597b702
Simon Haydon <shaydon@koimo.net>
2026-01-14 22:18:41 +0000

    Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import tinaDirective from "./astro-tina-directive/register"
+import tinaDirective from "./astro-tina-directive/register";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL || `https://${process.env.VERCEL_URL}`,
-	integrations: [mdx(), sitemap(), react(), tinaDirective()],
+  site: process.env.SITE_URL || `https://${process.env.VERCEL_URL}`,
+  integrations: [mdx(), sitemap(), react(), tinaDirective()],
+  adapter: vercel(),
 });
