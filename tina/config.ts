d9dac85dbc2a0ac5f1c77ba9923f45acd2c6f1d0
Simon Haydon <shaydon@koimo.net>
2026-01-15 01:20:56 +0000

    Configure repo-based media

--- tina/config.ts
+++ tina/config.ts
@@ -24,8 +24,9 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "",
       publicFolder: "public",
+      mediaRoot: "uploads",
+      static: false,
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
